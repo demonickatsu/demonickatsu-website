3c29844fae7b23a71cc48fa39212a2db230a4c13
Katsu <64220236+demonickatsu@users.noreply.github.com>
2022-09-26 16:49:28 -0400

    added all files
    added everything, including https://github.com/demonickatsu/metric-converter

---
+++ javascript-apps/imperial-converter/app.js
@@ -0,0 +1,9 @@
+let toConvert;
+
+document.getElementById("myButton").onclick = function(){
+    
+    toConvert = document.getElementById("myText").value;
+    console.log(toConvert);
+    document.getElementById("myLabel").innerHTML = toConvert / 2.54;
+}
+
