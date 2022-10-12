ecfb1f1e0445bf8e8a25168285ea4a0d27aace70
Katsu <64220236+demonickatsu@users.noreply.github.com>
2022-10-11 21:36:42 -0400

    🎉Added weather app

---
+++ javascript-apps/weather/app.js
@@ -0,0 +1,32 @@
+let API_KEY = "61d52278c9b8102403b731030720a238";
+
+getWeatherData = (city) => {
+  const URL = "https://api.openweathermap.org/data/2.5/weather";
+  const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`;
+  const weatherPromise  = fetch(FULL_URL);
+  return weatherPromise.then((response) => {
+    return response.json();
+  })
+}
+
+searchCity = () => {
+  const city = document.getElementById('city-input').value;
+  getWeatherData(city)
+  .then((res)=>{
+    showWeatherData(res);
+  }).catch((error)=>{
+    console.log(error);
+    console.log("An error occured");
+  })
+}
+
+showWeatherData = (weatherData) => {
+  document.getElementById("city-name").innerText = weatherData.name;
+  document.getElementById("weather-type").innerText = weatherData.weather[0].main;
+  document.getElementById("temp").innerText = weatherData.main.temp;
+  document.getElementById("min-temp").innerText = weatherData.main.temp_min;
+  document.getElementById("max-temp").innerText = weatherData.main.temp_max;
+  document.getElementById("feel").innerText = weatherData.main.feels_like;
+  document.getElementById("weather-output").classList.add('visible');
+}
+
